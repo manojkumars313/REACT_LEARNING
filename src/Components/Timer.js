6b65d246d5d01b83d00375433fdfe6c30d030938
manojkumars313 <manojraj.mm313@gmail.com>
2023-02-20 14:38:32 +0530

    Function_state

---
+++ src/Components/Timer.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+
+const Timer = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = React.useRef(null);
+
+  const startTimer = () => {
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
+      setTime((prevTime) => {
+        if (prevTime >= 30) {
+          clearInterval(intervalRef.current);
+          setIsRunning(false);
+          return 30;
+        } else {
+          return prevTime + 1;
+        }
+      });
+    }, 1000);
+  };
+
+  const pauseTimer = () => {
+    clearInterval(intervalRef.current);
+    setIsRunning(false);
+  };
+
+  const restartTimer = () => {
+    clearInterval(intervalRef.current);
+    setTime(0);
+    setIsRunning(false);
+  };
+
+  useEffect(() => {
+    return () => clearInterval(intervalRef.current);
+  }, []);
+
+  const seconds = `0${time % 60}`.slice(-2);
+  const minutes = `0${Math.floor(time / 60)}`;
+
+  return (
+    <div>
+      <h2>
+        {minutes}:{seconds}
+      </h2>
+      {isRunning ? (
+        <button onClick={pauseTimer}>Pause</button>
+      ) : (
+        <button onClick={startTimer}>Start</button>
+      )}
+      <button onClick={restartTimer}>Restart</button>
+    </div>
+  );
+};
+
+export default Timer;
