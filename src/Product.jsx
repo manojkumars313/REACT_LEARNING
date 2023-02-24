e94d65ce2c2735c44f63010ef650f32e27b5577b
manojkumars313 <manojraj.mm313@gmail.com>
2023-02-24 15:40:31 +0530

    ObjectOfArray

---
+++ src/Product.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Product = ({productName , price}) => {
+  return (
+      <div>
+          
+          
+          <div> 
+           
+            <h2>{productName }</h2>
+            <h3>price :{price }</h3>
+            </div>
+    </div>
+  )
+}
+
+export default Product
