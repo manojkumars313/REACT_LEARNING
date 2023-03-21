ebc754e6ecf6d3092d4a7ec5c8b3b3d1945c8469
manojkumars313 <manojraj.mm313@gmail.com>
2023-03-21 05:51:00 +0530

    r

---
+++ src/Components/Add.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { increamentCounterAction } from "../Redux/Actions/CounterAction";
+import { decrementCounterAction } from "../Redux/Actions/CounterAction";
+
+function Add() {
+  return (
+    <div>
+      <h1>Add</h1>
+    </div>
+  );
+}
+
+export default Add;
